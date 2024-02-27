799d605822a2dba66770ee174a4ab45d7d1555d5
AleBustamante <adbdc.u@gmail.com>
2024-02-26 21:59:31 -0400

    Funciona para anios multiplos de 400

--- src/bisiesto.spec.js
+++ src/bisiesto.spec.js
@@ -5,4 +5,7 @@ describe("AnioBisiesto", () => {
     it("Deberia dar verdadero para años bisiestos. Caso anio 400", () => {
         expect(esBisiesto(400)).toEqual(true);
     });
+    it("Deberia dar verdadero para años bisiestos. Caso anio 800", () => {
+        expect(esBisiesto(800)).toEqual(true);
+    });
 });
