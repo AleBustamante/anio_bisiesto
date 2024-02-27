47cf1994eec1b0764c600f7b699e553a30d4008a
AleBustamante <adbdc.u@gmail.com>
2024-02-26 22:04:19 -0400

    Funciona para anios no bisiestos por ser multiplos de 100 pero no de 400

--- src/bisiesto.js
+++ src/bisiesto.js
@@ -2,6 +2,9 @@ function esBisiesto(anio) {
     if (anio % 400 === 0) {
         return true;
     }
+    else if (anio % 100 && anio % 400 != 0) {
+        return false;
+    }
     return false;
 }
 
--- src/bisiesto.spec.js
+++ src/bisiesto.spec.js
@@ -8,4 +8,13 @@ describe("AnioBisiesto", () => {
     it("Deberia dar verdadero para años bisiestos. Caso anio 800", () => {
         expect(esBisiesto(800)).toEqual(true);
     });
+    it("Deberia dar falso para años no bisiestos. Caso anio 401", () => {
+        expect(esBisiesto(401)).toEqual(false);
+    });
+    it("Deberia dar falso para años no bisiestos. Caso anio 1700", () => {
+        expect(esBisiesto(1700)).toEqual(false);
+    });
+    it("Deberia dar falso para años no bisiestos. Caso anio 2100", () => {
+        expect(esBisiesto(2100)).toEqual(false);
+    });
 });
